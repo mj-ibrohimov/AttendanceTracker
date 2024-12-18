d7b01fab632fef97c0c38d78459fd144f12f7c57
Muhammadjon_Ibrohimov <Muhammadjon_Ibrohimov@student.itpu.uz>
2024-12-18 13:44:57 +0100

    Initial Commit

---
+++ AirBadge/AirBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBadgeApp.swift
+//  AirBadge
+//
+//  Created by Muhammadjon on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
